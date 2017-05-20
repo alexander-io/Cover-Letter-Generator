e539342e9e83cd6f162b82ad0a96a9e82f81cec6
claybeard <abharris@pugetsound.edu>
2017-05-19 21:53:32 -0700

    organize, add comment, remove  extraneous

--- cover_generator.py
+++ cover_generator.py
@@ -20,11 +20,13 @@
 
     # python3 cover_generator.py -t Data Analytics Engineer -mn Dorothy Potterhaven -mp Hiring Manager -cn PACCAR -ca 381 Empire Street -cc Seattle -cs Washington -cz 98411
 
-import sys, os
-import collections
+# get tools
+import sys, os, collections
 
+# declare globals
 global company_name, company_street_address, company_city, company_state, company_zip_code, job_title, hiring_manager_name, hiring_manager_position
 
+# set globals to null
 company_name = company_street_address = company_city = company_state = company_zip_code = job_title = hiring_manager_name = hiring_manager_position = None
 
 # XXX test print XXX
@@ -42,6 +44,7 @@ tags_dict['-t'] = job_title
 tags_dict['-mn'] = hiring_manager_name
 tags_dict['-mp'] = hiring_manager_position
 
+# make a dictionary of strings associated with each tag for the hell of it
 global var_names
 var_names = collections.OrderedDict()
 var_names['-cn'] = 'company name'
@@ -52,32 +55,6 @@ var_names['-cz'] = 'company zip code'
 var_names['-t'] = 'job title'
 var_names['-mn'] = 'hiring manager name'
 var_names['-mp'] = 'hiring manager position'
-
-
-# global tags_index
-# tags_index = collections.OrderedDict()
-# tags_index['-cn'] = 0
-# tags_index['-ca'] = 1
-# tags_index['-cc'] = 2
-# tags_index['-cs'] = 3
-# tags_index['-cz'] = 4
-# tags_index['-t']  = 5
-# tags_index['-mn'] = 6
-# tags_index['-mp'] = 7
-
-# tags = {
-#     '-cn' : company_name,
-#     '-ca' : company_street_address,
-#     '-cc' : company_city,
-#     '-cs' : company_state,
-#     '-cz' : company_zip_code,
-#     '-t' : job_title,
-#     '-mn' : hiring_manager_name,
-#     '-mp' : hiring_manager_position
-# }
-
-# for x in tags_dict:
-#     print(x, tags_dict[x])
 
 # loop through the arguments provided by the user
 for alpha in sys.argv:
@@ -100,52 +77,23 @@ for alpha in sys.argv:
 
             i = index_of_matching_tag+1
             while i < len(sys.argv):
+                # if we've reached the next tag, break
                 if '-' in sys.argv[i]:
                     break
+                # otherwise, append the string at i to the string builder
                 string_builder += sys.argv[i] + " "
                 i+=1
-
-
+            # we've built the string of values between tags in the array,
+            # now it's time to store them in the global dictionary
             tags_dict[alpha] = string_builder
-            # print('string builder : ', string_builder)
-
-            # for x in sys.argv:
-            #
-            #     print(x)
-            # print('found a matching tag', alpha, 'equals', beta
 
 
-
+# XXX test print the values in the XXX
 for x in tags_dict:
     if tags_dict[x] == None:
         pass
     else:
         print(var_names[x], ':', tags_dict[x])
-
-    # print(x)
-
-
-# if len(sys.argv)>=4:
-#     if (sys.argv[1] == 'help'):
-#
-#         # if (sys.argv[1] == 'help'):
-#         print('Usage : \n\t$ python3 unit-conv.py <number_of_units> <unit_type>\n\t$ python3 unit-conv.py 1024 bits\n')
-#     else:
-#         # try to parse user input for the number of bytes and unit type
-#         try:
-#             company_name = int(sys.argv[1])
-#         except:
-#             num_bytes = -1
-#             print('Usage : \n\t$ python3 unit-conv.py <number_of_units> <unit_type>\n\t$ python3 unit-conv.py 1024 bits\n')
-#             pass
-#
-# try:
-#     if (isinstance(sys.argv[2], str)):
-#         unit = sys.argv[2]
-# except:
-#     pass
-
-
 
 
 # probs won't need this if the user passes in command line args
